80cbe411090a0cca91f8653bc8ae63003f346b2d
Andres Lemos <Lemos_1325@hotmail.com>
2023-09-16 21:20:00 -0500

    feat: Se ajusta el servicio de implementacion, se crea el metodo getById y tambien se añade la etiqueta @Transactional.

--- src/main/java/com/app/clientcontrolback/services/ClientService.java
+++ src/main/java/com/app/clientcontrolback/services/ClientService.java
@@ -5,31 +5,34 @@ import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.app.clientcontrolback.interfaces.IClientService;
-import com.app.clientcontrolback.interfaces.IClientDao;
+import com.app.clientcontrolback.dao.ClientDao;
 import com.app.clientcontrolback.models.Client;
 
 @Service
 public class ClientService implements IClientService {
 	
 	@Autowired
-	private IClientDao repo;
+	private ClientDao clientDao;
 	
 	public static final Logger LOGGER = Logger.getLogger(ClientService.class.getName());
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Client> listClient() {
-		return this.repo.findAll();
+		return (List<Client>) this.clientDao.findAll();
 	}
 
 	@Override
-	public String createClient(Client client) {
+	@Transactional
+	public String saveClient(Client client) {
 		String response = "";
 		
 		try {
 			ClientService.LOGGER.info("Processing Client data to save.");
-			this.repo.save(client);
+			this.clientDao.save(client);
 			response = "OK";
 			
 		} catch(Exception e) {
@@ -41,35 +44,35 @@ public class ClientService implements IClientService {
 	}
 
 	@Override
-	public String editClient(Client client) {
-		String response = "";
-		
-		try {
-			ClientService.LOGGER.info("Processing Client data to edit.");
-			
-			this.repo.save(client);
-			response = "OK";
-			
-		} catch(Exception e) {
-			ClientService.LOGGER.warning("Error!! processing Client data to edit: " + e);
-			response = "ERROR";
-		}
-		
-		return response;
-	}
-
-	@Override
+	@Transactional
 	public String deleteClient(Integer id) {
 		String response = "";
 		
 		try {
 			ClientService.LOGGER.info("Processing Client data to delete.");
 			
-			this.repo.deleteById(id);
+			this.clientDao.deleteById(id);
 			response = "OK";
 			
 		} catch(Exception e) {
 			ClientService.LOGGER.warning("Error!! processing Client data to delete: " + e);
+			response = "ERROR";
+		}
+		
+		return null;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Client getById(Integer id) {
+		String response = "";
+		
+		try {		
+			this.clientDao.findById(id);
+			response = "OK";
+			
+		} catch(Exception e) {
+			ClientService.LOGGER.warning("Error!! processing Client data to getById: " + e);
 			response = "ERROR";
 		}
 		
